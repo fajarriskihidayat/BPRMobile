37beabd43578823979af966594d12385342b8f05
Ferryndraa03 <ferryndrasatryawan03@gmail.com>
2023-08-14 19:16:12 +0700

    ui login, register, Simulasi ready

---
+++ frontend/src/screens/Kredit.js
@@ -0,0 +1,247 @@
+import { 
+    View, 
+    Text,
+    Image,
+    StyleSheet, 
+    TextInput,
+    TouchableOpacity,
+    ImageBackground,
+    ToastAndroid,
+    ScrollView,
+  } from 'react-native'
+  import React, { useState, useEffect } from 'react'
+  import Logo from '../assets/Logo.png'
+  import { useNavigation } from '@react-navigation/native'
+  import axios from 'axios'
+  import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+  import DropDownPicker from 'react-native-dropdown-picker';
+  import Modal from "react-native-modal";
+  import Office from '../assets/DBS.png'
+
+const Kredit = () => {
+
+  const [bunga,setBunga] = React.useState('');
+  const [plafond,setPlafond] = React.useState('');
+  const [tenor,setTenor] = React.useState('');
+  const [angsuran,setAngsuran] = React.useState('');
+  const [isModalVisible, setModalVisible] = useState(false);
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
+
+  const [open, setOpen] = useState(false);
+      const [value, setValue] = useState(null);
+      const [items, setItems] = useState([
+        {label: '8%', value: 8},
+        {label: '10%', value: 10}
+      ]);
+
+      const result = async(data) => {
+        let plafond = data.plafond;
+        let tenor = data.tenor;
+        let hasil = (parseInt(plafond) + (parseInt(plafond)*((data.value/100) * (parseInt(tenor)/12))))/parseInt(tenor);
+        // console.log("angsuran per bulan = ", Math.round(hasil));
+        let hasill = Math.round(hasil);
+        setAngsuran(hasill)
+      }
+
+  return (
+    <View style={styles.container}>
+          <View style={styles.navbar}>
+      <Image source={Logo} style={styles.logo}/>
+      </View>
+        <View style={styles.body}>
+        <View style={{alignItems:'center'}}>
+        <Text style={styles.textTitle}>Simulasi Kredit</Text>
+        </View>
+        <Image source={Office} style={styles.image}/>
+        <Text style={styles.formText}>Jenis Kredit</Text>
+        <DropDownPicker
+      open={open}
+      value={value}
+      onSelectItem={(bunga) => setBunga(bunga)}
+      items={items}
+      setOpen={setOpen}
+      setValue={setValue}
+      setItems={setItems}
+      placeholderStyle={{
+          color: "grey",
+        }}
+      style={styles.dropDown}
+      placeholder="Pilih kategori" 
+
+        />
+
+      <Text style={styles.formText}>Plafond Kredit</Text>
+         <TextInput
+         style={styles.input}
+         placeholder ="Plafond Kredit"
+         placeholderTextColor={'#969595'}
+         onChangeText={(plafond) => setPlafond(plafond)}
+         value={plafond}
+         />
+
+      <Text style={styles.formText}>Tenor/Jangka Waktu</Text>
+         <TextInput
+         style={styles.input}
+         placeholder ="Tenor"
+         placeholderTextColor={'#969595'}
+         onChangeText={(tenor) => setTenor(tenor)}
+         value={tenor}
+         />
+
+
+        <TouchableOpacity style={styles.button}
+          onPress={async () => { 
+            result(
+              {
+                value : value, 
+                plafond : plafond, 
+                tenor: tenor
+              })
+              setModalVisible(true)
+            }
+          }
+        >
+          <Text style={{color:'white',fontWeight:'700', fontSize: 18}}>Hitung</Text>
+        </TouchableOpacity>
+        <Modal
+        isVisible={isModalVisible}
+        animationIn = "slideInUp"
+        animationOut= "zoomOut"
+        animationOutTiming = {300}
+        backdropTransitionOutTiming = {200}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.textSuccess}>
+              Submit Success
+            </Text>
+            <View style={{marginVertical : 20}}>
+            <Text>Jenis Kredit : </Text>
+            <Text>Plafond Kredit : Rp. {plafond}</Text>
+            <Text>Jangka Waktu : {tenor} Bulan</Text>
+              <Text>Angsuran per bulan : Rp. {angsuran}</Text>
+              </View>
+            <TouchableOpacity style={styles.buttonClose} onPress={toggleModal}>
+                <Text style={styles.btnCloseText}>Ok</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        </Modal>
+        </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container : {
+      flex : 1,
+      backgroundColor: 'white',
+    },
+    logo : {
+      width : 270,
+      height : 20,
+      marginTop : 5
+    },
+    navbar : {
+      backgroundColor : '#fc5453',
+      height : 50,
+      paddingLeft : 20,
+      flexDirection : 'row',
+      paddingVertical : 10,
+    },
+    header : {
+      paddingLeft : 20,
+      paddingTop : 20,
+      flexDirection : 'row',
+      marginTop : 20
+    },
+    title : {
+      color : 'white',
+      fontWeight : '700',
+      fontSize : 27, 
+    },
+    body : {
+      paddingHorizontal : 30,
+      paddingTop : 20,
+    },
+    button : {
+      alignItems : 'center',
+      justifyContent : 'center',
+      backgroundColor : '#fc5453',
+      height : 45,
+      borderRadius : 15,
+      marginTop : 20
+    },
+    image : {
+      width : 330,
+      height : 100,
+      borderRadius : 20,
+      marginTop : 20
+    },
+    formText : {
+      fontSize : 15,
+      fontWeight : '300',
+      color : 'black',
+      marginTop : 20
+    },
+    textTitle : {
+      fontSize : 26,
+      fontWeight : '700',
+      color : 'black'
+    },
+    input : {
+        height : 50,
+        borderWidth : 1,
+        borderColor : 'black',
+        borderRadius : 10,
+        borderWidth : 1,
+        paddingHorizontal : 10,
+        fontSize : 17,
+        fontWeight : '300',
+        marginVertical : 10,
+        color : 'black'
+      },
+      dropDown : {
+        marginTop : 10
+      },
+      modalView: {
+        marginHorizontal: 20,
+        backgroundColor: "white",
+        alignItems : 'center',
+        borderRadius: 5,
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5
+      },
+        buttonClose: {
+          backgroundColor: "#1dd1a1",
+          width : 150,
+          alignItems : 'center',
+          justifyContent : 'center',
+          height : 40,
+          borderRadius : 20,
+          marginVertical : 20,
+      
+        },
+        btnCloseText : {
+          color : 'white',
+          fontWeight : 'bold',
+          fontSize : 16,
+        },
+      
+        textSuccess : {
+          color : '#1dd1a1',
+          fontSize : 24,
+          fontWeight : 'bold',
+          marginVertical : 10,
+        }
+})
+
+export default Kredit
---
+++ frontend/src/screens/Register.js
@@ -0,0 +1,189 @@
+import { 
+  View, 
+  Text,
+  Image,
+  StyleSheet, 
+  TextInput,
+  TouchableOpacity,
+  ImageBackground,
+  ToastAndroid
+} from 'react-native'
+import React, { useState, useEffect } from 'react'
+// // import Illustration from '../assets/main.png'
+
+import { useNavigation } from '@react-navigation/native'
+// import axios from 'axios'
+
+
+const Register = () => {
+  const navigation = useNavigation();
+  // const [nim,setNim] = React.useState('');
+  // const [nama,setNama] = React.useState('');
+  // const [password,setPassword] = React.useState('');
+
+  // const handleRegis = async(value) => {
+  //   console.log('value', value);
+
+  //   try {
+  //     const response = await axios.post('http://192.168.130.109:3800/mahasiswa/',{
+  //       nim : value.nim,
+  //       nama : value.nama,
+  //       password : value.password
+  //     })
+  //     if(response.data.status == 200){
+  //       console.log('response', response.data);
+  //       navigation.navigate('Login')
+  //       ToastAndroid.show(response.data.metadata, ToastAndroid.SHORT)
+  //       ToastAndroid.show("Sign Up Success", ToastAndroid.SHORT)
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //     ToastAndroid.show("Cek kembali data pendaftaran", ToastAndroid.SHORT)
+  //   }
+
+  // }
+
+
+  return (
+    <View style={styles.container}>
+     {/* <ImageBackground source={Poster} resizeMode="cover"> */}
+      <View style={styles.body}>
+          <Text style={styles.title1}>Make an new account</Text>
+          {/* <Image source={Illustration} style={styles.Illustration}/> */}
+      </View>
+      <View style={styles.form}>
+      <Text style={styles.formText}>Nama</Text>
+        <TextInput
+        style={styles.input}
+        placeholder ="Nama"
+        placeholderTextColor="#58565e"
+        // onChangeText={(nama) => setNama(nama)}
+        // value={nama}
+        />
+      <Text style={styles.formText}>Username</Text>
+        <TextInput
+        style={styles.input}
+        placeholder ="Username"
+        placeholderTextColor="#58565e"
+        // onChangeText={(nama) => setNama(nama)}
+        // value={nama}
+        />
+        <Text style={styles.formText}>No. Hp</Text>
+        <TextInput
+        style={styles.input}
+        placeholder ="No. Hp"
+        placeholderTextColor="#58565e"
+        // onChangeText={(nim) => setNim(nim)}
+        // value={nim}
+        />
+        <Text style={styles.formText}>Password</Text>
+        <TextInput
+        style={styles.input}
+        secureTextEntry={true}
+        placeholder ="Password"
+        placeholderTextColor="#58565e"
+        // onChangeText={(password) => setPassword(password)}
+        // value={password}
+        />
+
+      </View>
+      <View style={styles.footer}>
+        <TouchableOpacity style = {styles.button} 
+          // onPress={async() => await handleRegis({nim, nama, password})}
+          >
+            <Text style = {styles.buttonText}>Buat Akun</Text>
+        </TouchableOpacity>
+        <Text style = {styles.lowerText}>
+          Already have an account? 
+          <Text style ={{fontWeight : 'bold',color:'#fc5453'}} 
+          onPress={() => navigation.navigate('Login')}
+          >
+           Sign In here
+        </Text>
+        </Text>
+      </View>
+      {/* </ImageBackground> */}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container : {
+    flex : 1,
+    backgroundColor: 'white',
+  },
+  topBar : {
+    backgroundColor : '#443C68',
+    alignItems : 'center',
+    justifyContent : 'center',
+    width : 400,
+    height : 60,
+  },
+  Logo : {
+    width : 110,
+    height : 30,
+  },
+  body : {
+    alignItems : 'center',
+    
+  },
+  Illustration : {
+    marginTop : 25,
+    width : 170,
+    height : 180,
+  },
+  title1 : {
+    fontSize : 27,
+    fontWeight : '700',
+    color : '#323333',
+    marginTop: 40
+  },
+  form : {
+    marginTop : 30,
+    marginHorizontal: 50,
+  },
+  formText : {
+    fontSize : 18,
+    fontWeight : '600',
+    color : '#323333',
+  },
+  input : {
+      width :300,
+      height : 50,
+      borderWidth : 1,
+      backgroundColor : '#e0e0e0',
+    //   borderColor : '#BABABA',
+      borderRadius : 10,
+      borderWidth : 0,
+      color : '#323333',
+      paddingHorizontal : 20,
+      fontSize : 17,
+      fontWeight : '300',
+      marginVertical : 15,
+    },
+  footer : {
+    alignItems : 'center',
+  },
+  button : {
+      width :300,
+      height : 50,
+      backgroundColor : '#fc5453',
+      borderRadius : 10,
+      justifyContent : 'center',
+      alignItems : 'center',
+      marginTop: 20,
+    },
+  buttonText :{
+    color : '#FFF',
+    fontSize :20,
+    fontWeight : 'bold', 
+  },
+  lowerText : {
+    color : '#323333',
+    fontSize : 16,
+    marginTop : 20,
+  }
+
+})
+
+export default Register
